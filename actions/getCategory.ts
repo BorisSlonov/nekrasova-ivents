4e6c74c73f375c67de37bebed68e489df7989329
BorisSlonov <sslonovborisss@gmail.com>
2024-07-04 23:44:31 +0300

    fix endpoint

--- actions/getCategory.ts
+++ actions/getCategory.ts
@@ -2,7 +2,7 @@ import { ICategoriesCard } from "@/components/CategoriesList/types";
 
 export const getCategory = async (slug: string): Promise<ICategoriesCard | undefined> => {
   try {
-    const result = await fetch(`${process.env.API_URL}good/?populate=*&filters[categories][slug]=${slug}&pagination[page]=1&pagination[pageSize]=1000`, {
+    const result = await fetch(`${process.env.API_URL}good/?populate=*&filters[cats][slug]=${slug}&pagination[page]=1&pagination[pageSize]=1000`, {
       next: { revalidate: 60 },
     });
 
